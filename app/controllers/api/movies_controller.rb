50a56b1ce4a37a148a2640286b0614b36ac815af
Grant Kazan <grantkazan@gmail.com>
2019-04-15 18:48:27 -0500

    added other RESTful actions for movies

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -3,4 +3,33 @@ class Api::MoviesController < ApplicationController
     @movies = Movie.all
     render "index.json.jbuilder"
   end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def create 
+    @movie = Movie.new(
+      title: params[:name],
+      runtime: params[:runtime]
+      )
+    @movie.save
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.title = params[:title] || @movie.title
+    @movie.runtime = params[:runtime] || @movie.runtime
+    @movie.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render json: {message: "Movie destroyed"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get "/movies" => "movies#index"
+
+    get "/movies/:id" => "movies#show"
+
+    post "/movies" => "movies#create"
+
+    post "/movies/:id" => "movies#update"
+
+    delete "/movies/:id" => "movies#destroy"
   end
 end
