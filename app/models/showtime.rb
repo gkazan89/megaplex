6422acb86ae05084c348159ed8a5f4fecfbe35d8
Grant Kazan <grantkazan@gmail.com>
2019-04-21 11:58:07 -0500

    added sold out model method to showtime

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -2,4 +2,14 @@ class Showtime < ApplicationRecord
   belongs_to :theater
   belongs_to :movie
   has_many :tickets
+
+  def sold_out
+    capacity = self.theater.capacity
+    tix = self.tickets.length
+    if tix >= capacity
+      sold_out = {status: true}
+    else
+      sold_out = {status: false}
+    end
+  end
 end
--- app/views/api/showtimes/_showtime.json.jbuilder
+++ app/views/api/showtimes/_showtime.json.jbuilder
@@ -1,3 +1,7 @@
+json.id showtime.id
 json.title showtime.movie.title
 json.theater_id showtime.theater_id
 json.time showtime.time
+json.capacity showtime.theater.capacity
+json.tickets_bought  showtime.tickets.length
+json.sold_out showtime.sold_out
